4bf9377408aa41367d451a8d94ff916d918fc210
lizhh666 <axionlzh@gmail.com>
2017-04-28 16:53:29 +0800

    delete join

--- control/models.py
+++ control/models.py
@@ -7,7 +7,12 @@ class Person(AbstractUser):
     name = models.CharField(u'昵称', max_length=10)
     college = models.CharField(u'学院',max_length=10)
     IDnum = models.CharField(u'学号',max_length=10, unique=True)
-    join = models.ManyToManyField(u'参加社团',Community)
+    #join = models.ManyToManyField(u'参加社团',Community)
 
 class Community(models.Model):
     name = models.CharField(u'名字', max_length=20, primary_key=True)
+    #member = models.ManyToManyField(u'成员', Person)
+
+'''class Person_Community:
+    Perosn
+    Community'''
